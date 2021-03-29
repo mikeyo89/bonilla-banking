64a0a7dc99b9615bd9b62f8b393d44d8500078e8
Michael Bonilla <Mikeyo89@icloud.com>
2021-03-29 03:41:02 -0400

    Added template html, urls, and views.

--- bonilla_banking/landing_site/urls.py
+++ bonilla_banking/landing_site/urls.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('learn-more/', LearnView.as_view(), name='learn'),
+    path('support/', SupportView.as_view(), name='support'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('sign-up/', SignUpView.as_view(), name='sign-up'),
 ]
--- bonilla_banking/landing_site/views.py
+++ bonilla_banking/landing_site/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = 'index.html'
+
+class LearnView(generic.TemplateView):
+    template_name = 'learn.html'
+
+class SupportView(generic.TemplateView):
+    template_name = 'support.html'
+
+class LoginView(generic.TemplateView):
+    template_name = 'login.html'
+
+class SignUpView(generic.CreateView):
+    template_name = 'signup.html'
