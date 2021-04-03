908342a3cc10f3c08b2ac477923b1edc650312f5
Michael Bonilla <Mikeyo89@icloud.com>
2021-04-03 17:46:08 -0400

    added user login and signup
    Added user login and signup pages, forms, and views.

--- bonilla_banking/landing_site/views.py
+++ bonilla_banking/landing_site/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.messages.views import SuccessMessageMixin
+
 from django.views import generic
+from django.contrib.auth import views, authenticate, login
 
 from .models import SupportModel
-from .forms import SupportForm
+from .forms import SupportForm, AccountForm, LoginForm
+from django.contrib.auth import get_user_model
 
 class IndexView(generic.TemplateView):
     """ View for the index, or landing page, of Bonilla Banking. """
@@ -28,23 +31,36 @@ class SupportView(generic.CreateView):
         return super().form_valid(self.object)
 
     def get_success_url(self):
-        return reverse('success')
+        return reverse('landing:success')
 
     def get_error_url(self):
-        return reverse('support')
+        return reverse('landing:support')
 
 class SuccessView(generic.TemplateView):
     """ View for success page. Landing page of the SupportView. """
     template_name = 'success.html'
 
-class LoginView(generic.TemplateView):
+class LoginView(views.LoginView):
     """
-    View for the Login page. Verifies that user exists and passes that along to Dashboard view.
+    View for the Login page. Verifies that user exists and passes that along to Dashboard view in the main_site app.
     """
-    template_name = 'login.html'
+    template_name = 'accounts/login.html'
 
-class SignUpView(generic.CreateView):
+    def get_success_url(self):
+        return reverse('main:dashboard')
+
+
+class SignUpView(SuccessMessageMixin, generic.CreateView):
     """
     View for the sign-up page. Verifies form data and, if successful, creates a new user in the Users model.
     """
-    template_name = 'signup.html'
+    template_name = 'accounts/signup.html'
+    model = get_user_model()
+    form_class = AccountForm
+    success_message = 'Account created successfully.'
+
+    def get_success_url(self):
+        return reverse('main:dashboard')
+    
+    def get_error_url(self):
+        return reverse('landing:sign-up')
